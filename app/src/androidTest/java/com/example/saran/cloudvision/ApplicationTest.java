88fcfcd3b2426202b784a9ec5c73c47c08ba1aeb
sarangvmk <giddalurusaran@gmail.com>
2017-03-07 22:44:51 -0800

    test case added

--- app/src/androidTest/java/com/example/saran/cloudvision/ApplicationTest.java
+++ app/src/androidTest/java/com/example/saran/cloudvision/ApplicationTest.java
@@ -2,7 +2,7 @@ package com.example.saran.cloudvision;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
+import static org.junit.Assert.*;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -10,4 +10,10 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+   @Test
+   public void testvalue() throws Exception {
+    
+    assertTrue(False);
+   }
 }
